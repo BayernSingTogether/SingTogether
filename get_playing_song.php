775cec5ca7fb16c6bb56f104e678a01c5cf62c7d
xbj110825 <99568323@qq.com>
2018-01-20 22:32:18 +0800

    echo cookie

--- get_playing_song.php
+++ get_playing_song.php
@@ -27,7 +27,7 @@ try {
     $res   = $conn->query($query);
     $row   = $res->fetch();
     $song_length = $row['song_length'];
-    /*
+    
     if ($song_length * 1000 + $soom_playing_song_timestrap > get_millisecond()) {
         //update playing song
         $query = "SELECT user_vote,count(*) FROM user GROUP BY user_vote ORDER BY count(*) DESC limit 1";
@@ -38,8 +38,8 @@ try {
         $res   = $conn->query($query);
     }
     
-    $query = "UPDATE room SET room_playing_song_id = ".$row[0].", soom_playing_song_timestrap = ".get_millisecond()." WHERE room_id = 1";
-    $res   = $conn->query($query);*/
+    #$query = "UPDATE room SET room_playing_song_id = ".$row[0].", soom_playing_song_timestrap = ".get_millisecond()." WHERE room_id = 1";
+    #$res   = $conn->query($query);
     
     $json['ret'] = true;
     $json['msg'] = '';
