af11ad67ecedb2c3f4b2c7f59dad045863ee9809
xbj110825 <99568323@qq.com>
2018-01-20 19:23:56 +0800

    echo cookie

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ echo '?';
 
 if (!isset($_COOKIE["user"])) {
     
-    /*try {
+    try {
         $conn  = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $query = "insert into users values()";
@@ -20,23 +20,21 @@ if (!isset($_COOKIE["user"])) {
         $res   = $conn->exec($query);
         $row = $res->fetch(); 
         print_r($row);
+        $json['ret'] = true;
+        $json['msg'] = 'test';
         #setcookie("cookie_name", "cookie_value", time() + 99 * 365 * 24 * 3600);
     } catch(PDOException $e) {
         $json['ret'] = false;
         $json['msg'] = $e->getMessage();
-        exit();
-    }*/
-    echo '1';
-    $json['ret'] = true;
-    $json['msg'] = 'test';
+    }
+    
     
 } else {
-    echo '2';
     $json['ret'] = true;
     $json['msg']    = 'cookie user already exist';
 }
 
-#header('Content-type: application/json');
-#echo json_encode($json, JSON_UNESCAPED_UNICODE);
+header('Content-type: application/json');
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
 
 ?>
