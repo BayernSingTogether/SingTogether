db372051d9fa4e816d4a527f1fa81056a4a389d0
xbj110825 <99568323@qq.com>
2018-01-20 18:53:45 +0800

    echo cookie

--- login.php
+++ login.php
@@ -1,12 +1,17 @@
 <?php
 
-require('common/config.php');
+ini_set('display_errors', 'On');
+define("DB_HOST",'m60mxazb4g6sb4nn.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306'); //Host base de datos MySql
+define("DB_USER", 'g48fqaog7yhlic5p'); //Nombre de la base de datos
+define("DB_PASS" , 'l4l4wkcad1im3kbg'); //Contrase?a de la base de datos MySql
+define("DB_NAME" , 'yw8uospcgbz3woi1'); //Nombre de la base de datos server final
+
 #require('common/error.php');
 
-#if (isset($_COOKIE["user"]) {
-#    echo 'isset'.$_COOKIE["user"];
-#} else {
-#    echo 'nosset';
-#}
+if (isset($_COOKIE["user"]) {
+    echo 'isset'.$_COOKIE["user"];
+} else {
+    echo 'nosset';
+}
 
 ?>
