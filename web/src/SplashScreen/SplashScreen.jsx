a618ba3617479975c319ecf57b00b77e8f20bda9
Pep Rodeja Ferrer <pepjo26@gmail.com>
2018-01-20 16:48:40 +0000

    New splash screen

--- web/src/SplashScreen/SplashScreen.jsx
+++ web/src/SplashScreen/SplashScreen.jsx
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
+
 import './SplashScreen.css';
 
 class SplashScreen extends Component {
   render() {
     return (
-      <div className="SplashScreen">
-        <h1>
-          Wilkommen!
-        </h1>
-        
-        <button onClick={this.props.onClick}>
-          BRING IT ON!
-        </button>
-      </div>
+      <div className="SplashScreen" onClick={this.props.onClick} />
     );
   }
 }
