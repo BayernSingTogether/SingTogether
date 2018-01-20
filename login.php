7b25a3e3cf438c3867ad608912db335d3ac3196c
xbj110825 <99568323@qq.com>
2018-01-20 18:37:44 +0800

    echo cookie user

--- login.php
+++ login.php
@@ -2,22 +2,7 @@
 
 require('common/config.php');
 require('common/error.php');
-require('common/param.php');
 
-function GET_cb()
-{
-    global $method;
-    
-    $user = get_param($method, 'user');
-    echo $user;
-}
-
-$method    = $_SERVER['REQUEST_METHOD'];
-$method_cb = $method.'_cb';
-if (function_exists($method_cb)) {
-    call_user_func($method_cb);
-} else {
-    handle_error('request methon error');
-}
+echo $_COOKIE["user"];
 
 ?>
