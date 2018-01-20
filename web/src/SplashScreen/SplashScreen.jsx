e5a0aafd454dd2ac0d06fbed4fe4476b814e2317
Pep Rodeja Ferrer <pepjo26@gmail.com>
2018-01-20 16:57:22 +0000

    Fix background

--- web/src/SplashScreen/SplashScreen.jsx
+++ web/src/SplashScreen/SplashScreen.jsx
@@ -5,7 +5,7 @@ import './SplashScreen.css';
 class SplashScreen extends Component {
   render() {
     return (
-      <div className="SplashScreen" onClick={this.props.onClick} />
+      <div className="SplashScreen" style={{ height: window.innerHeight }} onClick={this.props.onClick} />
     );
   }
 }
