e0a96c3e8c8d610db35b2463d0ca1af1a33ca19e
xbj110825 <99568323@qq.com>
2018-01-20 18:51:49 +0800

    echo cookie

--- login.php
+++ login.php
@@ -3,10 +3,10 @@
 require('common/config.php');
 #require('common/error.php');
 
-if(isset($_COOKIE["user"]){
-    echo 'isset'.$_COOKIE["user"];
-}else{
-    echo 'nosset';
-}
+#if (isset($_COOKIE["user"]) {
+#    echo 'isset'.$_COOKIE["user"];
+#} else {
+#    echo 'nosset';
+#}
 
 ?>
