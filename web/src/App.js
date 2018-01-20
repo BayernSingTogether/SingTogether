f1f9eafcdd04944def779fa6651f4561264ea74c
Pep Rodeja Ferrer <pepjo26@gmail.com>
2018-01-20 21:47:43 +0000

    Test working version

--- web/src/App.js
+++ web/src/App.js
@@ -17,6 +17,11 @@ class App extends Component {
     this.getSongsList = this.getSongsList.bind(this)
     this.postVote = this.postVote.bind(this)
     this.getPlayingSong = this.getPlayingSong.bind(this)
+    this.downloadSongAndLyrics = this.downloadSongAndLyrics.bind(this)
+    this.downloadSong = this.downloadSong.bind(this)
+    this.downloadLyrics = this.downloadLyrics.bind(this)
+    this.setPlayingSong = this.setPlayingSong.bind(this)
+    this.playAudio = this.playAudio.bind(this)
     
     this.state = {
       currentView: 'splashscreen',
@@ -25,18 +30,25 @@ class App extends Component {
       songs: [],
       playingStarted: null,
       playingSong: null,
+      nextSong: null,
+      nextSongBlob: null,
+      currentLine: 0,
+      lyrics: [],
+      nextLyrics: [],
+      currentTime: 0,
     }
 
     const x = setInterval(() => {
       this.getSongsList()
+      this.getPlayingSong()
       
       /// TEST MOCKUP
       this.setState({
-        currentTime: this.state.currentTime + 1
+        currentTime: this.state.currentTime + 1.5
       })
-    }, 1000);
+    }, 1500);
     
-    this.getPlayingSong();
+    this.serverTime = window.ServerDate()
   }
   
   getSongsList () {
@@ -59,10 +71,29 @@ class App extends Component {
   getPlayingSong () {
     axios.get('/api/v1/get_playing_song.php')
     .then((response) => {
-      this.setState({
-        playingStarted: parseInt((response.data || {}).room_playing_song_timestrap, 10),
-        playingSong: parseInt((response.data || {}).room_playing_song_id)
-      })
+      const playingStarted = parseInt((response.data || {}).room_playing_song_timestrap, 10)
+      const playingSong = parseInt((response.data || {}).room_playing_song_id)
+      const nextSong = parseInt((response.data || {}).room_next_song_id)
+      
+      if (playingSong !== this.state.playingSong && this.state.songs.length !== 0) {
+        if (playingSong === this.state.nextSong) {
+          this.setState({
+            lyrics: this.state.nextLyrics
+          })
+          this.setPlayingSong(this.state.nextSongBlob)
+          this.playAudio(playingStarted)
+          this.downloadSongAndLyrics('next', nextSong)
+        } else {
+          this.downloadSongAndLyrics('current', playingSong)
+          this.downloadSongAndLyrics('next', nextSong)
+        }
+
+        this.setState({
+          playingStarted,
+          playingSong,
+          nextSong,
+        })
+      }
     })
     .catch((error) => {
       console.log(error)
@@ -76,12 +107,79 @@ class App extends Component {
       currentVote: songId
     })
   }
+  
+  setPlayingSong (blob) {
+    this.audio.src = window.URL.createObjectURL(blob)
+  }
+  
+  downloadSongAndLyrics (songType, songData) {
+    this.downloadSong(songType, `/public/${songData.song_file}`)
+    this.downloadLyrics(songType, `/public/${songData.song_lyric}`)
+  }
+
+  downloadSong (songType, url) {
+    axios.get(url, { responseType: 'blob' })
+    .then((response) => {
+      if (songType === 'current') {
+        this.setPlayingSong(response.data)
+      } else {
+        this.setState({
+          nextSongBlob: response.data
+        })
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  downloadLyrics (songType, url) {
+    this.setState({
+      lyrics: []
+    })
+
+    axios.get(url)
+    .then((response) => {
+      if (songType === 'current') {
+        let currentLine = false
+        response.data.forEach((item, i) => {
+          if (currentLine === false && item[0] > this.state.currentTime) {
+            currentLine = i
+          }
+        })
+
+        this.setState({
+          lyrics: response.data || [],
+          currentLine,
+        })
+      } else {
+        this.setState({
+          nextLyrics: response.data || [],
+        })
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+  
+  playAudio () {
+    console.log('server time', this.state.playingStarted, 'local time', this.serverTime.UTC())
+
+    this.audio.play()
+    
+    const time = this.serverTime.UTC() - this.state.playingStarted
+    this.audio.currentTime = time / 1000
+  }
 
   render() {
     if (this.state.currentView === 'splashscreen') {
       return (
         <div>
-          <SplashScreen onClick={() => ( this.setState({ currentView: 'songlist' }) ) } />
+          <SplashScreen onClick={() => {
+            this.setState({ currentView: 'songlist' })
+            this.playAudio()
+          }} />
         </div>
       );
     } else if (this.state.currentView === 'songlist') {
@@ -93,10 +191,12 @@ class App extends Component {
             currentVote={this.state.currentVote}
             postVote={this.postVote}
           />
+          <audio ref={(ref) => (this.audio = ref)} />
           <Player
             currentTime={this.state.currentTime}
             currentSong={this.state.songs.find((item) => (item.song_id === this.state.playingSong)) || {}}
-            playingStarted={this.state.playingStarted}
+            currentLine={this.state.currentLine}
+            lyrics={this.state.lyrics}
           />
         </div>
       );
--- web/src/Player/Player.jsx
+++ web/src/Player/Player.jsx
@@ -4,97 +4,18 @@ import axios from 'axios'
 import './Player.css'
 
 class Player extends Component {
-  constructor(props) {
-    super(props)
-    
-    this.downloadSongAndLyrics = this.downloadSongAndLyrics.bind(this)
-    this.downloadSong = this.downloadSong.bind(this)
-    this.downloadLyrics = this.downloadLyrics.bind(this)
-    
-    this.state = {
-      currentLine: 0,
-      lyrics: [],
-      currentTime: 0,
-    }
-
-    if (this.props.currentSong) {
-      this.downloadSongAndLyrics(this.props.currentSong)
-    }
-  }
-  
-  downloadSongAndLyrics (songData) {
-    this.downloadSong(`/public/${songData.song_file}`)
-    this.downloadLyrics(`/public/${songData.song_lyric}`)
-  }
-
-  downloadSong (url) {
-    axios.get(url, { responseType: 'blob' })
-    .then((response) => {
-      const song = window.URL.createObjectURL(response.data)
-      
-      this.audio.src = song
-      this.audio.play()
-      
-      const time = performance.timing.navigationStart + performance.now() - this.props.playingStarted
-      
-      console.log('Started at ', time, 'server time', this.props.playingStarted, 'local time', performance.timing.navigationStart + performance.now())
-      
-      this.audio.currentTime = time / 1000
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
-
-  downloadLyrics (url) {
-    this.setState({
-      lyrics: []
-    })
-
-    axios.get(url)
-    .then((response) => {
-      let currentLine = false
-      response.data.forEach((item, i) => {
-        if (currentLine === false && item[0] > this.props.currentTime) {
-          currentLine = i
-        }
-      })
-
-      this.setState({
-        lyrics: response.data || [],
-        currentLine,
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
-
-  
-  shouldComponentUpdate (nextProps, nextState) {
-    let shouldUpdate = false
-    
-    if (nextProps.currentSong.id !== this.props.currentSong.id) {
-      shouldUpdate = true
-      this.downloadSongAndLyrics(nextProps.currentSong)
-    }
-    
-    return shouldUpdate
-  }
-  
   render() {
     return (
       <nav class="playing">
-        <audio ref={(ref) => (this.audio = ref)} />
         <div class="playing__song">
           <div class="playing__artist">{this.props.currentSong.song_artist}</div>
           <div class="playing__name">{this.props.currentSong.song_name}</div>
           <div class="playing__ball" style={{ left: window.innerWidth * this.props.currentTime / parseFloat(this.props.currentSong.song_length) }} />
         </div>
         <div class="playing__lyrics">
-          <div class="playing__lyrics__scroll" style={{ marginTop: 20-29*this.state.currentLine }}>
-            {this.state.lyrics.map((item, i) => {
-              if (this.state.currentLine === i) {
+          <div class="playing__lyrics__scroll" style={{ marginTop: 20-29*this.props.currentLine }}>
+            {this.props.lyrics.map((item, i) => {
+              if (this.props.currentLine === i) {
                 return (
                   <div key={i} class="playing__lyrics__line playing__lyrics__line--current">{item[1]}</div>
                 )
