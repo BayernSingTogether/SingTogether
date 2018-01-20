98408c04d8bd5646800615308bd20a78218ab67e
xbj110825 <99568323@qq.com>
2018-01-20 19:17:32 +0800

    echo cookie

--- login.php
+++ login.php
@@ -6,14 +6,34 @@ define("DB_USER", 'g48fqaog7yhlic5p'); //Nombre de la base de datos
 define("DB_PASS" , 'l4l4wkcad1im3kbg'); //Contrase?a de la base de datos MySql
 define("DB_NAME" , 'yw8uospcgbz3woi1'); //Nombre de la base de datos server final
 
-#require('common/error.php');
+$json = array();
+#$json['status'] = true;
+#$json['openid'] = $obj->openid;
 
-echo 'login.php';
-
-if (isset($_COOKIE["user"])) {
-    echo 'isset'.$_COOKIE["user"];
+if (!isset($_COOKIE["user"])) {
+    
+    try {
+        $conn  = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $query = "insert into users values()";
+        $res   = $conn->exec($query);
+        $query = "SELECT LAST_INSERT_ID()";
+        $res   = $conn->exec($query);
+        $row = $res->fetch(); 
+        print_r($row);
+        #setcookie("cookie_name", "cookie_value", time() + 99 * 365 * 24 * 3600);
+    } catch(PDOException $e) {
+        $json['ret'] = false;
+        $json['msg'] = $e->getMessage();
+        exit();
+    }
+    
 } else {
-    echo 'nosset';
+    $json['ret'] = true;
+    $json['msg']    = 'cookie user already exist';
 }
+
+header('Content-type: application/json');
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
 
 ?>
