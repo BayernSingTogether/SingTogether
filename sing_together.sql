379023dd8ae27a9c3cbde6fee503ff3e1e9e0746
xbj110825 <99568323@qq.com>
2018-01-20 20:49:06 +0800

    echo cookie

--- sing_together.sql
+++ sing_together.sql
@@ -1,40 +1,25 @@
-DROP database IF EXISTS sing_together;
-CREATE database sing_together;
+DROP database IF EXISTS yw8uospcgbz3woi1;
+CREATE database yw8uospcgbz3woi1;
 
-USE sing_together;
+USE yw8uospcgbz3woi1;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `user_name`            VARCHAR(64) NOT NULL,
-  `user_room_id_entered` INT(11)     DEFAULT NULL,
-  `user_song_id_voted`   INT(11)     DEFAULT NULL,
-  PRIMARY KEY (`user_name`)
-) CHARSET = utf8;
+  `user_id`     INT(11) NOT NULL AUTO_INCREMENT,
+  `user_vote`   INT(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) CHARSET=utf8;
 
 DROP TABLE IF EXISTS `song`;
-CREATE TABLE `song`(
-    `song_id`            INT(11)      NOT NULL AUTO_INCREMENT,
-    `song_name`          VARCHAR(100) NOT NULL,
-    `song_length`        INT(11)      NOT NULL,
-    `song_file`          VARCHAR(100) NOT NULL,
-    `song_lyric`         VARCHAR(100) NOT NULL,
-    PRIMARY KEY (song_id)
-) CHARSET = utf8;
+CREATE TABLE `song` (
+  `song_id`     INT(11)      NOT NULL AUTO_INCREMENT,
+  `song_length` INT(11)      NOT NULL,
+  `song_name`   VARCHAR(100) NOT NULL,
+  `song_file`   VARCHAR(100) NOT NULL,
+  `song_lyric`  VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`song_id`)
+) CHARSET=utf8;
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `user_id`              INT(11)     NOT NULL AUTO_INCREMENT,
-  `user_name`            VARCHAR(64) NOT NULL,
-  `user_room_id_entered` INT(11)     DEFAULT NULL,
-  `user_song_id_voted`   INT(11)     DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) CHARSET = utf8;
+INSERT INTO `song`(song_length, song_name, song_file, song_lyric) VALUES(225, 'Forever Number One', 'Forever Number One.mp3', 'Forever Number One.lrc');
+INSERT INTO `song`(song_length, song_name, song_file, song_lyric) VALUES(225, 'Lemon Tree'        , 'Lemon Tree.mp3'        , 'Lemon Tree.lrc'        );
 
-DROP TABLE IF EXISTS `room`;
-CREATE TABLE `room` (
-    `room_id`                    INT(11)     NOT NULL AUTO_INCREMENT,
-    `room_name`                  VARCHAR(64) NOT NULL,
-    `room_song_id_playing`       INT(11)     NOT NULL,
-    `room_song_starts_timestrap` VARCHAR(64) NOT NULL,
-    PRIMARY KEY (`user_id`)
-) CHARSET = utf8;
