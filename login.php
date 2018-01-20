6f02f7267734ee3baa3955614978e79d1cc4cd20
xbj110825 <99568323@qq.com>
2018-01-20 21:53:29 +0800

    echo cookie

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if (!isset($_COOKIE["user"])) {
     try {
         $conn  = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $query = "INSERT INTO user(user_vote) values(1)";
+        $query = "INSERT INTO user(user_vote) VALUES(1)";
         $res   = $conn->query($query);
         $query = "SELECT LAST_INSERT_ID()";
         $res   = $conn->query($query);
