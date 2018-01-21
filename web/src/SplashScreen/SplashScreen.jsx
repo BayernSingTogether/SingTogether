072e843c8026f9f064288926bf2c05a195050daa
Pep Rodeja Ferrer <pepjo26@gmail.com>
2018-01-21 17:26:04 +0000

    Fix everything

--- web/src/SplashScreen/SplashScreen.jsx
+++ web/src/SplashScreen/SplashScreen.jsx
@@ -6,6 +6,8 @@ class SplashScreen extends Component {
   constructor(props) {
     super(props)
     
+    this.handleClick = this.handleClick.bind(this)
+    
     this.state = {
       syncstate: 0,
       clicked: false,
