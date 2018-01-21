cc2523028ffbd391c44cb7cad599940af83a2bbb
Pep Rodeja Ferrer <pepjo26@gmail.com>
2018-01-21 09:57:57 +0000

    improve speed sync

--- web/src/index.js
+++ web/src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Accelerate sync
+window.ServerDate.amortizationRate = 100
+
 ReactDOM.render(<App />, document.getElementById('root'));
