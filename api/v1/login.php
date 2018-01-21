b44300722dbad9fa8b408c6dbe45ea7b8ff8d4bc
xbj110825 <99568323@qq.com>
2018-01-21 18:29:51 +0800

    echo cookie

--- api/v1/login.php
+++ api/v1/login.php
@@ -1,10 +1,23 @@
 <?php
 require('global.php');
 $json = array();
-if (!isset($_COOKIE['user'])) {
-    try {
-        $conn  = new PDO('mysql:host='.setting::db_host.';dbname='.setting::db_name, setting::db_user, setting::db_pass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+    $conn  = new PDO('mysql:host='.setting::db_host.';dbname='.setting::db_name, setting::db_user, setting::db_pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $need_set_cookie = false;
+    if (!isset($_COOKIE['user'])) {
+        $need_set_cookie = true;
+    } else {
+        $query = 'SELECT * FROM user WHERE user_id='.$_COOKIE['user'];
+        $res   = $conn->query($query);
+        $row   = $res->fetch();
+        if ($row == NULL)
+            $need_set_cookie = true;
+    }
+    
+    if ($need_set_cookie == true) {
         $query = 'INSERT INTO user(user_vote) VALUES(NULL)';
         $res   = $conn->query($query);
         $query = 'SELECT LAST_INSERT_ID()';
@@ -13,13 +26,13 @@ if (!isset($_COOKIE['user'])) {
         $json['ret'] = true;
         $json['msg'] = 'cookie `user='.$row[0].'` set successfully';
         setcookie('user', $row[0], time() + 99 * 365 * 24 * 3600);
-    } catch(PDOException $e) {
-        $json['ret'] = false;
-        $json['msg'] = $e->getMessage();
+    } else {
+         $json['ret'] = true;
+         $json['msg'] = 'cookie `user='.$_COOKIE["user"].'` already exist';
     }
-} else {
-    $json['ret'] = true;
-    $json['msg'] = 'cookie `user='.$_COOKIE["user"].'` already exist';
+} catch(PDOException $e) {
+    $json['ret'] = false;
+    $json['msg'] = $e->getMessage();
 }
 echo json_encode($json, JSON_UNESCAPED_UNICODE);
 ?>
